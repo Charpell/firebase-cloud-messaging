06a8505d2ac49c694f69379e4808290aaa4748fd
Ebuka Umeh <charpellumeh@gmail.com>
2020-11-22 10:52:17 +0100

    setup firebase

--- js/main.js
+++ js/main.js
@@ -8,12 +8,21 @@ const FIREBASE_AUTH = firebase.auth();
 const FIREBASE_MESSAGING = firebase.messaging();
 const FIREBASE_DATABASE = firebase.database();
 
+const signInButton = document.getElementById('sign-in');
+const signOutButton = document.getElementById('sign-out');
+const subscribeButton = document.getElementById('subscribe');
+const unsubscribeButton = document.getElementById('unsubscribe');
+
 /* ========================
   Event Listeners
 ======================== */
 
+signInButton.addEventListener("click", signIn);
+
 /* ========================
   Functions
 ======================== */
-
+function signIn() {
+  FIREBASE_AUTH.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+}
 }
